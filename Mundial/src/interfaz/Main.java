69a46891a7d6cf6d8b99d3d3ca8c18a85226f223
escuela@davinci.edu.ar <escuela@davinci.edu.ar>
2022-11-14 15:14:42 -0300

    Sin errores
    Errores de la clase Partido solucionado
    (ya no salta ningun error)

--- Mundial/src/interfaz/Main.java
+++ Mundial/src/interfaz/Main.java
@@ -54,69 +54,39 @@ public class Main {
 		
 		
 		LinkedList<Pais> listaPaises = new LinkedList<Pais>();
-			listaPaises.add(qatar);
-			listaPaises.add(ecuador);
-			listaPaises.add(senegal);
-			listaPaises.add(netherlands);
-			listaPaises.add(england);
-			listaPaises.add(ir_iran);
-			listaPaises.add(usa);
-			listaPaises.add(wales);
-			listaPaises.add(argentina);
-			listaPaises.add(saudi_arabia);
-			listaPaises.add(mexico);
-			listaPaises.add(poland);
-			listaPaises.add(france);
-			listaPaises.add(australia);
-			listaPaises.add(denmark);
-			listaPaises.add(tunisia);
-			listaPaises.add(spain);
-			listaPaises.add(costa_rica);
-			listaPaises.add(germany);
-			listaPaises.add(japan);
-			listaPaises.add(belgium);
-			listaPaises.add(canada);
-			listaPaises.add(morocco);
-			listaPaises.add(croatia);
-			listaPaises.add(brazil);
-			listaPaises.add(serbia);
-			listaPaises.add(switzerland);
-			listaPaises.add(cameroon);
-			listaPaises.add(portugal);
-			listaPaises.add(ghana);
-			listaPaises.add(uruguay);
-			listaPaises.add(korea_republic);
-			
+//			Grupo A								Grupo E
+			listaPaises.add(qatar);				listaPaises.add(spain);
+			listaPaises.add(ecuador);			listaPaises.add(costa_rica);
+			listaPaises.add(senegal);			listaPaises.add(germany);
+			listaPaises.add(netherlands);		listaPaises.add(japan);
+//			Grupo B								Grupo F
+			listaPaises.add(england);			listaPaises.add(belgium);
+			listaPaises.add(ir_iran);			listaPaises.add(canada);
+			listaPaises.add(usa);				listaPaises.add(morocco);
+			listaPaises.add(wales);				listaPaises.add(croatia);	
+//			Grupo C								Grupo G
+			listaPaises.add(argentina);			listaPaises.add(brazil);	
+			listaPaises.add(saudi_arabia);		listaPaises.add(serbia);
+			listaPaises.add(mexico);			listaPaises.add(switzerland);
+			listaPaises.add(poland);			listaPaises.add(cameroon);
+//			Grupo D								Grupo H
+			listaPaises.add(france);			listaPaises.add(portugal);
+			listaPaises.add(australia);			listaPaises.add(ghana);
+			listaPaises.add(denmark);			listaPaises.add(uruguay);
+			listaPaises.add(tunisia);			listaPaises.add(korea_republic);
+
 		Encargado e1 = new Encargado ("34704934", "Harry", "Mrtnz");
-		
-//			mostrarEquipos(listaPaises);
-//			buscarGrupo(listaPaises);
-//			buscarEquipo(listaPaises);
-			
-//		Partido pGA = new Partido ("90 minutos");
-//		Partido pGB = new Partido ("90 minutos");
-//		Partido pGC = new Partido ("90 minutos");
-//		Partido pGD = new Partido ("90 minutos");
-//		Partido pGE = new Partido ("90 minutos");
-//		Partido pGF = new Partido ("90 minutos");
-//		Partido pGG = new Partido ("90 minutos");
-//		Partido pGH = new Partido ("90 minutos");
-		
 //		e1.cargarResultado(p1);
+		
+//		mostrarEquipos(listaPaises);
+//		buscarGrupo(listaPaises);
+//		buscarEquipo(listaPaises);
 
-//		PARTIDOS DE GRUPO
-//		pGA.jugarFaceGrupos(qatar, ecuador, senegal, netherlands);
-//		pGB.jugarFaceGrupos(england, ir_iran, usa, wales);
-//		pGC.jugarFaceGrupos(argentina, saudi_arabia, mexico, poland);
-//		pGD.jugarFaceGrupos(france, australia, denmark, tunisia);
-//		pGE.jugarFaceGrupos(spain, costa_rica, germany, japan);
-//		pGF.jugarFaceGrupos(belgium, canada, morocco, croatia);
-//		pGG.jugarFaceGrupos(brazil, serbia, switzerland, cameroon);
-//		pGH.jugarFaceGrupos(portugal, ghana, uruguay, korea_republic);
-
-		mostrarCalificados(listaPaises);
+		//jugarFaceGrupos(qatar, ecuador, senegal, netherlands);
+		
+//		mostrarCalificados(listaPaises);
 //		jugarOctavos(listaPaises);
-		mostrarCalificados(listaPaises);
+//		mostrarCalificados(listaPaises);
 		
 		LinkedList<Pais> listaOctavos = new LinkedList<Pais>();
 		
@@ -125,115 +95,12 @@ public class Main {
 //			listaOctavos.add(partido.getPais1());
 //		}
 		
-	}
-	
-	public static void jugarOctavos(LinkedList<Pais> lista) {
-		int aux1, aux2, aux3, aux4, aux5, aux6, aux7, aux8,
-			aux9, aux10, aux11, aux12,aux13, aux14, aux15, aux16;
+		Partido partido = new Partido();
 		
-		aux1 =(int) (Math.random()*6 ); aux9 =(int) (Math.random()*6 );
-		aux2 =(int) (Math.random()*6 );	aux10 =(int) (Math.random()*6 );
-		aux3 =(int) (Math.random()*6 );	aux11 =(int) (Math.random()*6 );
-		aux4 =(int) (Math.random()*6 );	aux12 =(int) (Math.random()*6 );
-		aux5 =(int) (Math.random()*6 );	aux13 =(int) (Math.random()*6 );
-		aux6 =(int) (Math.random()*6 );	aux14 =(int) (Math.random()*6 );
-		aux7 =(int) (Math.random()*6 );	aux15 =(int) (Math.random()*6 );
-		aux8 =(int) (Math.random()*6 );	aux16 =(int) (Math.random()*6 );
-
-		if(lista.isEmpty()) {
-			System.out.println("Lista vacia pá");
-			
-		} else {
-			
-			for (Pais pais : lista) {
-				
-				if (pais.getGrupo() == "A"  && pais.getPuestoGrupo() == 1 && aux1 < aux2) {
-					pais.setCalificado(false); //Pierde queda eliminado
-				}
-				
-				if (pais.getGrupo() == "B"  && pais.getPuestoGrupo() == 2 && aux1 >= aux2) {
-					pais.setCalificado(false);
-				}
-				
-				if (pais.getGrupo() == "C"  && pais.getPuestoGrupo() == 1 && aux3 < aux4) {
-					pais.setCalificado(false);
-				}
-				
-				if (pais.getGrupo() == "D"  && pais.getPuestoGrupo() == 2 && aux3 >= aux4) {
-					pais.setCalificado(false);					
-				}
-				
-				if (pais.getGrupo() == "E"  && pais.getPuestoGrupo() == 1 && aux5 < aux6) {
-					pais.setCalificado(false);
-				}
-				
-				if (pais.getGrupo() == "F"  && pais.getPuestoGrupo() == 2 && aux5 >= aux6) {
-					pais.setCalificado(false);
-				}
-				
-				if (pais.getGrupo() == "G"  && pais.getPuestoGrupo() == 1 && aux7 < aux8) {
-					pais.setCalificado(false);
-				}
-				
-				if (pais.getGrupo() == "H"  && pais.getPuestoGrupo() == 2 && aux7 >= aux8) {
-					pais.setCalificado(false);
-				}
-				
-				if (pais.getGrupo() == "B"  && pais.getPuestoGrupo() == 1 && aux9 < aux10) {
-					pais.setCalificado(false);
-				}
 		
-				if (pais.getGrupo() == "A"  && pais.getPuestoGrupo() == 2 && aux9 >= aux10) {
-					pais.setCalificado(false);
-				}
-				
-				if (pais.getGrupo() == "D"  && pais.getPuestoGrupo() == 1 && aux11 < aux12) {
-					pais.setCalificado(false);
-				}
-				
-				if (pais.getGrupo() == "C"  && pais.getPuestoGrupo() == 2 && aux11 >= aux12) {
-					pais.setCalificado(false);
-				}
-				
-				if (pais.getGrupo() == "F"  && pais.getPuestoGrupo() == 1 && aux13 < aux14) {
-					pais.setCalificado(false);
-				}
-				
-				if (pais.getGrupo() == "E"  && pais.getPuestoGrupo() == 2 && aux13 >= aux14) {
-					pais.setCalificado(false);
-				}
-				
-				if (pais.getGrupo() == "H"  && pais.getPuestoGrupo() == 1 && aux15 < aux16) {
-					pais.setCalificado(false);			
-				}
-				
-				if (pais.getGrupo() == "G"  && pais.getPuestoGrupo() == 2 && aux15 >= aux16) {
-					pais.setCalificado(false);
-				}
-			}
-		}
-	}
-
-	public static void jugarCuartos(LinkedList<Pais> lista) {
-		int aux1, aux2, aux3, aux4, aux5, aux6, aux7, aux8;
-		
-		aux1 =(int) (Math.random()*6 ); aux2 =(int) (Math.random()*6 );
-		aux3 =(int) (Math.random()*6 ); aux4 =(int) (Math.random()*6 );
-		aux5 =(int) (Math.random()*6 ); aux6 =(int) (Math.random()*6 );
-		aux7 =(int) (Math.random()*6 ); aux8 =(int) (Math.random()*6 );
-		
-		if(lista.isEmpty()) {
-			System.out.println("Lista vacia pá");
+		for (int i = 0; i < listaPaises.size(); i+=4) {
 			
-		} else {
-			
-			for (Pais pais : lista) {
-				if (pais.isCalificado()) {
-
-				} else {
-
-				}
-			}
+			partido.jugarFaceGrupos(listaPaises.get(i), listaPaises.get(i+1), listaPaises.get(i+2), listaPaises.get(i+3));
 		}
 	}
 	
@@ -244,21 +111,6 @@ public class Main {
 		}else {
 			for (Pais pais : lista) {	
 				System.out.println("Equipo "+pais.getId()+ " = "+ pais.getNombre() + " | Grupo: " + pais.getGrupo());
-			}
-		}
-		System.out.println(); //Espacio separador
-	}
-	
-	public static void mostrarCalificados(LinkedList<Pais> lista){
-		if(lista.isEmpty()) {
-			System.out.println("Lista vacia pá");
-		
-		}else {
-			System.out.println("Paises clasificados a la siguiente fase:");
-			for (Pais pais : lista) {	
-				if (pais.isCalificado()) {
-					System.out.println(pais.getId()+" - "+pais.getNombre());
-				}
 			}
 		}
 		System.out.println(); //Espacio separador
@@ -294,16 +146,12 @@ public class Main {
 		String equipo = entrada.next();
 		
 		if(lista.isEmpty()) {
-			System.out.println("Lista vacia pá");
+			System.out.println( "Lista vacia pá");
 		}else {
 			for (Pais pais : lista) {
 				if(pais.getNombre().equalsIgnoreCase(equipo)) {
 					System.out.println("RESULTADO ENCONTRADO: \n"
-						+pais.getNombre()+ " - Grupo: " +pais.getGrupo() + " - Goles totales: "+pais.getGoles());
-//				}else {
-//					System.out.println("No se ha encontrado ningun resultado"
-//							+ "\nVuelve a intentar");
-//					buscarEquipo(listaPais);
+					+pais.getNombre()+ " - Grupo: " +pais.getGrupo() + " - Goles totales: "+pais.getGoles());
 				}
 	        }
 		}
--- Mundial/src/logica/Partido.java
+++ Mundial/src/logica/Partido.java
@@ -7,22 +7,30 @@ public class Partido {
 	private Pais pais1;
 	private Pais pais2;
 	private int duracion = 90;
-	private int extra;
-//	private String[] resultado = new String[8];
-	
-	
 
+	public Partido(int id, Pais pais1, Pais pais2, int duracion) {
+		super();
+		this.id = id;
+		this.pais1 = pais1;
+		this.pais2 = pais2;
+		this.duracion = duracion;
+	}
+	
+	public Partido() {
+
+	}
+	
 	@Override
 	public String toString() {
-		return "Partido [tiempo=" + duracion + "]";
+		return "Partido [id=" + id + ", pais1=" + pais1 + ", pais2=" + pais2 + ", duracion=" + duracion + "]";
+	}
+	
+	public int getId() {
+		return id;
 	}
 
-	public Partido( Pais pais1, Pais pais2, int duracion) {
-	super();
-	this.id = id++;
-	this.pais1 = pais1;
-	this.pais2 = pais2;
-	this.duracion = duracion;
+	public void setId(int id) {
+		this.id = id;
 	}
 
 	public Pais getPais1() {
@@ -40,21 +48,130 @@ public class Partido {
 	public void setPais2(Pais pais2) {
 		this.pais2 = pais2;
 	}
-//	
-//	extra = (int) (Math.random()*10 );
-//	duracion = duracion + entra;
-//	Partido partido = new Partido(pais1 ,pais2, duracion);
+
+	public int getDuracion() {
+		return duracion;
+	}
+
+	public void setDuracion(int duracion) {
+		this.duracion = duracion;
+	}
+
+//	Partido partido = new Partido(id++, pais1 ,pais2, duracion);
 	
+	LinkedList<Pais> listaOctavos = new LinkedList<Pais>();
+	LinkedList<Pais> listaCuartos = new LinkedList<Pais>();
+	LinkedList<Pais> listaSemifinal = new LinkedList<Pais>();
+	LinkedList<Pais> listaTercerPuesto = new LinkedList<Pais>();
+	LinkedList<Pais> listaFinal = new LinkedList<Pais>();
+	
+	LinkedList<Partido> Partidos = new LinkedList<Partido>();
+
 	public LinkedList<Pais> jugarFaceGrupos(Pais pais1, Pais pais2, Pais pais3, Pais pais4){
+		int gol1, gol2, gol3, gol4, tiempoExtra; //Goles de cada equipo
+		tiempoExtra =(int) (Math.random()*10 ); 
+
+		this.duracion = duracion + tiempoExtra; //Duracion total del partido
 		
-		//	listaOctavos.add(ganadores);
+//		System.out.println(this.pais1.getNombre()+" vs " +this.pais2.getNombre());
+		gol1 =(int) (Math.random()*6 );
+		gol2 =(int) (Math.random()*6 );
+//		System.out.println(this.pais1.getNombre()+ " ha metido " + gol1 + " goles");
+//		System.out.println(this.pais2.getNombre()+ " ha metido " + gol2 + " goles");
 		
+		if (gol1 > gol2) {
+			pais1.setPuntos(pais1.getPuntos()+3);
+//			System.out.println("Ha ganado " + pais1.getNombre()+"\n");
+		} else if(gol1 < gol2){
+			pais2.setPuntos(pais2.getPuntos()+3);
+//			System.out.println("Ha ganado " + pais2.getNombre()+"\n");
+		} else {
+			pais1.setPuntos(pais1.getPuntos()+1);
+			pais2.setPuntos(pais2.getPuntos()+1);
+//			System.out.println("Empate\n");
+		}	
+//		---------------------------------------------------------------------
+//		System.out.println(pais3.getNombre()+" vs " +pais4.getNombre());
+		gol3 =(int) (Math.random()*6 );
+		gol4 =(int) (Math.random()*6 );
+//		System.out.println(pais3.getNombre()+ " ha metido " + aux3 + " goles");
+//		System.out.println(pais4.getNombre()+ " ha metido " + aux4 + " goles");
+		
+//		System.out.println(pais1.getNombre()+" vs " +pais3.getNombre());
+		gol1 =(int) (Math.random()*6 );
+		gol3 =(int) (Math.random()*6 );
+//		System.out.println(pais1.getNombre()+ " ha metido " + aux1 + " goles");
+//		System.out.println(pais3.getNombre()+ " ha metido " + aux3 + " goles");
+		
+		if (gol1 > gol3) {
+			pais1.setPuntos(pais1.getPuntos()+3);
+//			System.out.println("Ha ganado " + pais1.getNombre()+"\n");
+		} else if(gol1 < gol3){
+			pais3.setPuntos(pais3.getPuntos()+3);
+//			System.out.println("Ha ganado " + pais3.getNombre()+"\n");
+		} else {
+			pais1.setPuntos(pais1.getPuntos()+1);
+			pais3.setPuntos(pais3.getPuntos()+1);
+//			System.out.println("Empate\n");
+		}
+//		---------------------------------------------------------------------
+//		System.out.println(pais4.getNombre()+" vs " +pais2.getNombre());
+		gol4 =(int) (Math.random()*6 );
+		gol2 =(int) (Math.random()*6 );
+//		System.out.println(pais4.getNombre()+ " ha metido " + aux4 + " goles");
+//		System.out.println(pais2.getNombre()+ " ha metido " + aux2 + " goles");
+		
+		if (gol4 > gol2) {
+			pais4.setPuntos(pais4.getPuntos()+3);
+//			System.out.println("Ha ganado " + pais4.getNombre()+"\n");
+		} else if(gol4 < gol2){
+			pais2.setPuntos(pais2.getPuntos()+3);
+//			System.out.println("Ha ganado " + pais2.getNombre()+"\n");
+		} else {
+			pais4.setPuntos(pais4.getPuntos()+1);
+			pais2.setPuntos(pais2.getPuntos()+1);
+//			System.out.println("Empate\n");
+		}
+//		---------------------------------------------------------------------
+//		System.out.println(pais4.getNombre()+" vs " +pais1.getNombre());
+		gol4 =(int) (Math.random()*6 );
+		gol1 =(int) (Math.random()*6 );
+//		System.out.println(pais4.getNombre()+ " ha metido " + aux4 + " goles");
+//		System.out.println(pais1.getNombre()+ " ha metido " + aux1 + " goles");
+		
+		if (gol4 > gol1) {
+			pais4.setPuntos(pais4.getPuntos()+3);
+//			System.out.println("Ha ganado " + pais4.getNombre()+"\n");
+		} else if(gol4 < gol1){
+			pais1.setPuntos(pais1.getPuntos()+3);
+//			System.out.println("Ha ganado " + pais1.getNombre()+"\n");
+		} else {
+			pais4.setPuntos(pais4.getPuntos()+1);
+			pais1.setPuntos(pais1.getPuntos()+1);
+//			System.out.println("Empate\n");
+		}
+//		---------------------------------------------------------------------
+//		System.out.println(pais2.getNombre()+" vs " +pais3.getNombre());
+		gol2 =(int) (Math.random()*6 );
+		gol3 =(int) (Math.random()*6 );
+//		System.out.println(pais2.getNombre()+ " ha metido " + aux2 + " goles");
+//		System.out.println(pais3.getNombre()+ " ha metido " + aux3 + " goles");
+		
+		if (gol2 > gol3) {
+			pais2.setPuntos(pais2.getPuntos()+3);
+//			System.out.println("Ha ganado " + pais2.getNombre()+"\n");
+		} else if(gol4 < gol3){
+			pais3.setPuntos(pais3.getPuntos()+3);
+//			System.out.println("Ha ganado " + pais3.getNombre()+"\n");
+		} else {
+			pais1.setPuntos(pais1.getPuntos()+1);
+			pais3.setPuntos(pais3.getPuntos()+1);
+//			System.out.println("Empate\n");
+		}
+		
+		calcularPuestoGrupos(pais1, pais2, pais3, pais4);
 			return listaOctavos;
 	}
-		
-	
-		LinkedList<Pais> listaOctavos = new LinkedList<Pais>();
-
 
 	public void jugarFaceGrupos2(Pais pais1, Pais pais2, Pais pais3, Pais pais4){
 		int aux1, aux2, aux3, aux4; //Goles de cada equipo
@@ -76,7 +193,6 @@ public class Partido {
 			pais2.setPuntos(pais2.getPuntos()+1);
 //			System.out.println("Empate\n");
 		}
-
 
 //		System.out.println(pais3.getNombre()+" vs " +pais4.getNombre());
 		aux3 =(int) (Math.random()*6 );
@@ -229,7 +345,7 @@ public class Partido {
 			pais4.setPuestoGrupo(1); pais3.setPuestoGrupo(2); pais1.setPuestoGrupo(3); pais2.setPuestoGrupo(4);
 		/* 24 */} else {
 			pais4.setPuestoGrupo(1); pais3.setPuestoGrupo(2); pais2.setPuestoGrupo(3); pais1.setPuestoGrupo(4);
-		}
+				}
 		
 		if (pais1.getPuestoGrupo() == 3 || pais1.getPuestoGrupo()== 4){
 			pais1.setCalificado(false);
@@ -243,7 +359,12 @@ public class Partido {
 		if (pais4.getPuestoGrupo() == 3 || pais4.getPuestoGrupo()== 4){
 			pais4.setCalificado(false);
 		}
-		
+
+		for (Pais pais : listaOctavos) {
+			if (pais.isCalificado()) {
+				listaOctavos.add(pais);
+		}
+
 //		System.out.println(pais1.getNombre()+ " consiguio el puesto " +pais1.getPuestoGrupo());
 //		System.out.println(pais2.getNombre()+ " consiguio el puesto " +pais2.getPuestoGrupo());
 //		System.out.println(pais3.getNombre()+ " consiguio el puesto " +pais3.getPuestoGrupo());
@@ -255,6 +376,6 @@ public class Partido {
 //		System.out.println(pais3.getNombre()+ " - calificacion: " +pais3.isCalificado());
 //		System.out.println(pais4.getNombre()+ " - calificacion: " +pais4.isCalificado());
 //		System.out.println("#####################\n");
+		}
 	}
-		
 }
